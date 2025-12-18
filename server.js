023de8f0058541583f2417c486c6e9785b19c54b
Julian Hart <julian.w.hart@gmail.com>
2025-12-19 11:46:06 +1300

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const server = express();
+const PORT = 3000;
+
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+
